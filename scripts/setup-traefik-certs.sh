3b9bac5839fe9b7d4b6b5417de1a5df21486e52f
Matt Black <mblackonline.git@gmail.com>
2025-10-05 21:02:07 -0400

    Removed obsolete scripts replaced by entrypoint.sh and traefik automation

--- scripts/setup-traefik-certs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source .env
-mkdir -p certs
-mkcert -cert-file certs/${DOMAIN}.pem \
-       -key-file certs/${DOMAIN}-key.pem \
-       ${DOMAIN} 127.0.0.1 ::1
-echo "Certificates generated for Traefik: certs/${DOMAIN}.pem and certs/${DOMAIN}-key.pem"
